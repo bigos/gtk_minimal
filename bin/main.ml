6d8f76dc6aab79a4d6c6001d3dafc6fb214632a0
Jacek Podkanski <ruby.object@googlemail.com>
2024-11-26 05:28:11 +0000

    still trying

--- bin/main.ml
+++ bin/main.ml
@@ -92,7 +92,8 @@ let widget_set_vexpand =
 (* https://docs.gtk.org/gtk4/callback.DrawingAreaDrawFunc.html *)
 (* Gtk4 gives me cairo_t but the cairo2 library wants context *)
 let cairo_draw_func _area cr0 _width _height _data =
-  let cr = Ctypes.(coerce (ptr cairo_t) (ptr context) cr0) in
+  let cr = Foreign.foreign_value "cairo_t" context cr0 in
+  (* let cr = Ctypes.(coerce (ptr cairo_t) (ptr context) cr0) in *)
   set_source_rgb cr 0.9 0.0 0.0 ;
   select_font_face cr "DejaVu Sans" ~weight:Bold ;
   set_font_size cr 1.2 ;
