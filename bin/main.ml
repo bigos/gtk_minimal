c276a681106cc5646d3eda72224a06c804dcf3f9
Jacek Podkanski <ruby.object@googlemail.com>
2025-03-03 01:11:34 +0000

    flags are toggled

--- bin/main.ml
+++ bin/main.ml
@@ -420,19 +420,9 @@ let imp_tile_coordinate_clear () =
   (* Printf.printf " clearinng tc \n%!" ; *)
   my_model := {!my_model with tc= None}
 
-let imp_uncover_field () =
-  Printf.printf "going to uncover field\n%!" ;
-  ()
-
-let imp_toggle_field_flag () =
-  Printf.printf "going to toggle field flag\n%!" ;
-  ()
-
 type mine_state = Empty | Mined
 
-type field_type = Covered
-
-(* | Flagged | Uncovered *)
+type field_type = Covered | Flagged (* | Uncovered *)
 
 type field = {mine_state: mine_state; field_type: field_type}
 
@@ -483,6 +473,25 @@ let new_matrix =
 
 let my_fields = ref new_matrix
 
+let imp_uncover_field () =
+  Printf.printf "going to uncover field\n%!" ;
+  ()
+
+let imp_toggle_field_flag () =
+  Printf.printf "going to toggle field flag\n%!" ;
+  let tc = !my_model.tc in
+  match tc with
+  | None ->
+      ()
+  | Some mtc ->
+      let current = Hashtbl.find !my_fields (mtc.y, mtc.x) in
+      let new_field_type =
+        match current.field_type with Covered -> Flagged | Flagged -> Covered
+      in
+      let newf = {mine_state= current.mine_state; field_type= new_field_type} in
+      Hashtbl.add !my_fields (mtc.y, mtc.x) newf ;
+      ()
+
 let draw_game_top_text cr =
   set_source_rgb cr 0.9 0.7 0.0 ;
   paint cr ;
@@ -505,14 +514,14 @@ let draw_game_top_text cr =
                  match field with
                  | {mine_state= Empty; field_type= Covered} ->
                      "EC"
-                 (* | {mine_state= Empty; field_type= Flagged} -> *)
-                 (*     "EF" *)
+                 | {mine_state= Empty; field_type= Flagged} ->
+                     "EF"
                  (* | {mine_state= Empty; field_type= Uncovered} -> *)
                  (*     "EU" *)
                  | {mine_state= Mined; field_type= Covered} ->
                      "MC"
-                 (* | {mine_state= Mined; field_type= Flagged} -> *)
-                 (*     "MF" *)
+                 | {mine_state= Mined; field_type= Flagged} ->
+                     "MF"
                  (* | {mine_state= Mined; field_type= Uncovered} -> *)
                  (*    "MU" *) ) )
   in
@@ -577,14 +586,14 @@ let draw_game_matrix cr =
             ( match field with
             | {mine_state= Empty; field_type= Covered} ->
                 minecolor
-            (* | {mine_state= Empty; field_type= Flagged} -> *)
-            (*     color2 cr *)
+            | {mine_state= Empty; field_type= Flagged} ->
+                color2 cr
             (* | {mine_state= Empty; field_type= Uncovered} -> *)
             (*     color2 cr *)
             | {mine_state= Mined; field_type= Covered} ->
                 set_source_rgb cr 0.9 0.0 0.5
-                (* | {mine_state= Mined; field_type= Flagged} -> *)
-                (*     color2 cr *)
+            | {mine_state= Mined; field_type= Flagged} ->
+                color2 cr
                 (* | {mine_state= Mined; field_type= Uncovered} -> *)
                 (*    color2 cr *) )
             (* go to location for ri ci *) ;
